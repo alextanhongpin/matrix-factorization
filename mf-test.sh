e43ad0fd40932b09609e7b95cd097dda74ea2760
alextanhongpin <alextan220990@gmail.com>
2024-01-25 01:53:04 +0800

    chore: update example

--- mf-test.sh
+++ mf-test.sh
@@ -2,7 +2,11 @@ FILE_PATH="ml-100k"
 
 # -t: testonly
 # -p: prediction output
-awk -F"\t" '{printf "%d |u %d |i %d\n", $3,$1,$2}' < $FILE_PATH/ua.test | \
-    poetry run python -m vowpalwabbit /dev/stdin \
-    -i movielens.reg \
-    -p p_out
+awk -F"\t" '{printf "%d |u %d |i %d\n", $3,$1,$2}' < $FILE_PATH/ua.test > test.data
+
+poetry run python -m vowpalwabbit \
+-d test.data \
+-i movielens.reg \
+-p p_out
+
+rm test.data
